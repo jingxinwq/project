1355466baa372aa79af14c7d7d55790cdf1f08dc
jing <651243131@qq.com>
2017-03-25 09:50:14 +0800

    ooo

--- front/src/components/quanqiugou/quanqiugou.vue
+++ front/src/components/quanqiugou/quanqiugou.vue
@@ -1,1 +1,23 @@
-全球购
+<template>
+  <div id="home">
+  	<keep-alive>
+			<router-view > </router-view>
+		</keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
