a8ee961ebb81cad25a256d6a363c7f3dc362089f
Jason <95396985@qq.com>
2017-03-29 20:29:21 +0800

    03292024 fixedbug

--- front/src/routerConfig.js
+++ front/src/routerConfig.js
@@ -1,37 +1,56 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-//import Todayzk from './components/todayzk/todayzk.vue'
-//import Quanqiugou from './components/quanqiugou/quanqiugou.vue'
-//import List from './components/quanqiugou/list.vue'
-//import Detail from './components/quanqiugou/detail.vue'
-//import Gouwuche from './components/gouwuche/gouwuche.vue'
-//import Myjuanpi from './components/myjuanpi/myjuanpi.vue'
-//import Zhifacang from './components/zhifacang/zhifacang.vue'
-const Today = resolve => require(["./components/todayzk/today.vue"], resolve)
-const Todayzk = resolve => require(["./components/todayzk/todayzk.vue"], resolve)
-const Quanqiugou = resolve => require(["./components/quanqiugou/quanqiugou.vue"], resolve)
-const List = resolve => require(["./components/quanqiugou/list.vue"], resolve)
 
-const Detail1 = resolve => require(["./components/todayzk/detail.vue"], resolve)
-const Detail2 = resolve => require(["./components/quanqiugou/detail.vue"], resolve)
+// const Today = resolve => require(["./components/todayzk/today.vue"], resolve)
+// const Todayzk = resolve => require(["./components/todayzk/todayzk.vue"], resolve)
+// const Quanqiugou = resolve => require(["./components/quanqiugou/quanqiugou.vue"], resolve)
+// const List = resolve => require(["./components/quanqiugou/list.vue"], resolve)
 
-const GoodsDetail1 = resolve => require(["./components/todayzk/goodsDetail.vue"], resolve)
-const GoodsDetail2 = resolve => require(["./components/quanqiugou/goodsDetail.vue"], resolve)
+// const Detail1 = resolve => require(["./components/todayzk/detail.vue"], resolve)
+// const Detail2 = resolve => require(["./components/quanqiugou/detail.vue"], resolve)
 
-const Gouwuche = resolve => require(["./components/gouwuche/gouwuche.vue"], resolve)
+// const GoodsDetail1 = resolve => require(["./components/todayzk/goodsDetail.vue"], resolve)
+// const GoodsDetail2 = resolve => require(["./components/quanqiugou/goodsDetail.vue"], resolve)
 
-const Myjuanpi = resolve => require(["./components/myjuanpi/myjuanpi.vue"], resolve)
+// const Gouwuche = resolve => require(["./components/gouwuche/gouwuche.vue"], resolve)
 
-const Zhifacang = resolve => require(["./components/zhifacang/zhifacang.vue"], resolve)
-const User = resolve => require(["./components/myjuanpi/user.vue"], resolve)
-const Login = resolve => require(["./components/myjuanpi/login.vue"], resolve)
-const Register = resolve => require(["./components/myjuanpi/register.vue"], resolve)
-const Userinfo = resolve => require(["./components/myjuanpi/userinfo.vue"], resolve)
-const Aboutme = resolve => require(["./components/myjuanpi/aboutme.vue"], resolve)
-const Callcenter = resolve => require(["./components/myjuanpi/callcenter.vue"], resolve)
-const Mycollect = resolve => require(["./components/myjuanpi/mycollect.vue"], resolve)
-const Order = resolve => require(["./components/myjuanpi/order.vue"], resolve)
+// const Myjuanpi = resolve => require(["./components/myjuanpi/myjuanpi.vue"], resolve)
+
+// const Zhifacang = resolve => require(["./components/zhifacang/zhifacang.vue"], resolve)
+// const User = resolve => require(["./components/myjuanpi/user.vue"], resolve)
+// const Login = resolve => require(["./components/myjuanpi/login.vue"], resolve)
+// const Register = resolve => require(["./components/myjuanpi/register.vue"], resolve)
+// const Userinfo = resolve => require(["./components/myjuanpi/userinfo.vue"], resolve)
+// const Aboutme = resolve => require(["./components/myjuanpi/aboutme.vue"], resolve)
+// const Callcenter = resolve => require(["./components/myjuanpi/callcenter.vue"], resolve)
+// const Mycollect = resolve => require(["./components/myjuanpi/mycollect.vue"], resolve)
+// const Order = resolve => require(["./components/myjuanpi/order.vue"], resolve)
+
+import Today from "./components/todayzk/today.vue"
+import Todayzk from "./components/todayzk/todayzk.vue"
+import Quanqiugou from"./components/quanqiugou/quanqiugou.vue"
+import List from"./components/quanqiugou/list.vue"
+
+import Detail1 from"./components/todayzk/detail.vue"
+import Detail2 from"./components/quanqiugou/detail.vue"
+
+import GoodsDetail1 from"./components/todayzk/goodsDetail.vue"
+import GoodsDetail2 from"./components/quanqiugou/goodsDetail.vue"
+
+import Gouwuche from"./components/gouwuche/gouwuche.vue"
+
+import Myjuanpi from"./components/myjuanpi/myjuanpi.vue"
+
+import Zhifacang from"./components/zhifacang/zhifacang.vue"
+import User from"./components/myjuanpi/user.vue"
+import Login from"./components/myjuanpi/login.vue"
+import Register from"./components/myjuanpi/register.vue"
+import Userinfo from"./components/myjuanpi/userinfo.vue"
+import Aboutme from"./components/myjuanpi/aboutme.vue"
+import Callcenter from"./components/myjuanpi/callcenter.vue"
+import Mycollect from"./components/myjuanpi/mycollect.vue"
+import Order from"./components/myjuanpi/order.vue"
 
 Vue.use(VueRouter)
 const routes=[
